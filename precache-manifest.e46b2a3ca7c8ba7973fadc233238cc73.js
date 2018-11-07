7a2e7c947fe1788542926c596e97cf013904628f
Wesley Ukadike <wesleyukadike@yahoo.com>
2018-11-07 10:48:42 +0100

    Updates

---
+++ precache-manifest.e46b2a3ca7c8ba7973fadc233238cc73.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "253010ce9600eb460538d99e91a0e0a9",
+    "url": "/Gtb-demo2/static/media/SON.253010ce.png"
+  },
+  {
+    "revision": "1a62fc849c32645e9e22",
+    "url": "/Gtb-demo2/static/css/main.c567ca97.chunk.css"
+  },
+  {
+    "revision": "94395bd701ef234769d23cd99d13db4f",
+    "url": "/Gtb-demo2/static/media/Nafdac_header.94395bd7.jpg"
+  },
+  {
+    "revision": "2f135b6f79c2c9c02b04",
+    "url": "/Gtb-demo2/static/js/1.2f135b6f.chunk.js"
+  },
+  {
+    "revision": "85c7bd22deb99f72a2ea",
+    "url": "/Gtb-demo2/static/js/runtime~main.85c7bd22.js"
+  },
+  {
+    "revision": "9c9e83ec2ec6f9ba8774c9b645caedb0",
+    "url": "/Gtb-demo2/static/media/cac.9c9e83ec.png"
+  },
+  {
+    "revision": "1a62fc849c32645e9e22",
+    "url": "/Gtb-demo2/static/js/main.1a62fc84.chunk.js"
+  },
+  {
+    "revision": "bc73dc9af5bfaba76a65fa5836610cba",
+    "url": "/Gtb-demo2/static/media/Page-12-Image-16.bc73dc9a.jpg"
+  },
+  {
+    "revision": "556120c27fe44366ad8271ca16637b0a",
+    "url": "/Gtb-demo2/static/media/gtblogo.556120c2.png"
+  },
+  {
+    "revision": "3f9b8a529f30ef829200bb3dcd5ce114",
+    "url": "/Gtb-demo2/static/media/Page-14-Image-32.3f9b8a52.jpg"
+  },
+  {
+    "revision": "979b38cb80fa7a1b89314b558d131789",
+    "url": "/Gtb-demo2/static/media/nafdac.979b38cb.png"
+  },
+  {
+    "revision": "2f135b6f79c2c9c02b04",
+    "url": "/Gtb-demo2/static/css/1.e219e4e1.chunk.css"
+  },
+  {
+    "revision": "737fe3afc5330bea1360e01fad4c41c7",
+    "url": "/Gtb-demo2/index.html"
+  }
+];
